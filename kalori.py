0948ae13165c19709fde85af4271c7805389d51e
Immortal-Hak3r <77427148+Immortal-Hak3r@users.noreply.github.com>
2022-10-19 22:08:46 +0800

    Add files via upload

---
+++ kalori.py
@@ -0,0 +1,48 @@
+print ("***CADANGAN JUMLAH KALORI YANG DIBENARKAN BERDASARKAN JANTINA DAN UMUR***")
+
+jantina = input("Adakah jantina anda lelaki? (Ya atau Tidak): ")
+
+if jantina == "Ya":
+    umur = int(input("Berapakah Umur Anda? "))
+
+    if (umur >= 60):
+        print ("Nilai kalori yang dibenarkan ialah 2010")
+
+    elif (umur >= 30):
+        print ("Nilai kalori yang dibenarkan ialah 2460")
+
+    elif (umur >= 19):
+        print ("Nilai kalori yang dibenarkan ialah 2440")
+
+    elif (umur >= 16):
+        print ("Nilai kalori yang dibenarkan ialah 2840")
+
+    elif (umur >= 13):
+        print ("Nilai kalori yang dibenarkan ialah 2690")
+
+    else:
+        print ("Maaf, had umur perlu 13 tahun dan ke atas.")
+
+elif jantina == "Tidak":
+    umur = int(input("Berapakah Umur Anda? "))
+
+    if (umur >= 60):
+        print ("Nilai kalori yang dibenarkan ialah 1780")
+
+    elif (umur >= 30):
+        print ("Nilai kalori yang dibenarkan ialah 2180")
+
+    elif (umur >= 19):
+        print ("Nilai kalori yang dibenarkan ialah 2000")
+
+    elif (umur >= 16):
+        print ("Nilai kalori yang dibenarkan ialah 2050")
+
+    elif (umur >= 13):
+        print ("Nilai kalori yang dibenarkan ialah 2180")
+
+    else:
+        print ("Maaf, had umur perlu 13 tahun dan ke atas.")
+
+else:
+    print("Terima Kasih")
